0a661478d3ed2a58359dfbe014d6aca71965ffe4
Marco Mercado Lugo <marcomercadolugo@customer-DGO-99-150.megared.net.mx>
2018-10-03 21:59:59 -0500

    Tracing the route from north-west as home and vertical steps

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ var cardinalPoints = {
     west: 270
 }
 
+const HEIGHT = 20;
+const CAMERA_TAKE = {
+    heigth: 60,
+    width: 100
+}
+
+var RECTANGLE_DIMENTIONS = {
+    heigth: 0,
+    width: 0
+}
+
 // Función de inicialización del mapa
 function initMap() {
     // Configuración inicial del mapa, almacenado en una variable global
@@ -41,7 +52,7 @@ function initMap() {
             polygon.push(point);
         });
 
-        
+
         console.log(polygonPerim);
         console.log(MapsLib.distanceOfRoute(polygonPerim));
         console.log("Area of polygon: " + MapsLib.areaOf(polygon).toFixed(3) + " mts2")
@@ -60,69 +71,110 @@ function initMap() {
     google.maps.event.addListener(drawingManager, 'rectanglecomplete', function (poly) {
 
         //Marcadores de las esquinas del rectangulo
-        var posMarkerNorth = {
+        var posMarkerNorthEast = {
             lat: poly.getBounds().getNorthEast().lat(),
             lng: poly.getBounds().getNorthEast().lng()
         }
 
-        var markerNorth = new google.maps.Marker({
-            position: posMarkerNorth,
-            map: map,
-        });
-        var posMarkerSouth = {
+
+        var posMarkerSouthWest = {
             lat: poly.getBounds().getSouthWest().lat(),
             lng: poly.getBounds().getSouthWest().lng()
         }
 
-        var markerSouth = new google.maps.Marker({
-            position: posMarkerSouth,
+        var posMarkerSouthEast = objectToJSON(MapsLib.intersection(LatLng.objectToLatLng(posMarkerSouthWest),
+            cardinalPoints.east,
+            LatLng.objectToLatLng(posMarkerNorthEast),
+            cardinalPoints.south));
+
+        var posMarkerNorthWest = objectToJSON(MapsLib.intersection(LatLng.objectToLatLng(posMarkerNorthEast),
+            cardinalPoints.west,
+            LatLng.objectToLatLng(posMarkerSouthWest),
+            cardinalPoints.north));
+
+
+
+        /* var markerNorthEast = new google.maps.Marker({
+            position: posMarkerNorthEast,
             map: map,
         });
+
+        var markerSouthWest = new google.maps.Marker({
+            position: posMarkerSouthWest,
+            map: map,
+        });
+
+        var markerNorthWest = new google.maps.Marker({
+            position: posMarkerNorthWest,
+            map: map,
+        });
+
+        var markerSouthEast = new google.maps.Marker({
+            position: posMarkerSouthEast,
+            map: map,
+        }); */
 
         // Linea diagonal del rectangulo
         var diagonalPath = [
-            posMarkerNorth,
-            posMarkerSouth,
+            posMarkerNorthEast,
+            posMarkerSouthWest,
         ];
-
-
-        var diagonal = new google.maps.Polyline({
-            path: diagonalPath,
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-            map: map
-        });
-
+        var home = posMarkerNorthEast;
         // Dibujar una linea sabiendo solo su punto inicial, la longitud y la dirección a la cúal queremos dibujar
-        var distance_in_meter = 300;
-        var bearing = cardinalPoints.west;
+        // Set up de la ruta
+        var drone_position = home;
 
-        //var start = new LatLon(posMarkerNorth.lat, posMarkerNorth.lng);
+        drone_position = MapsLib.drawStep(CAMERA_TAKE.width / 2, cardinalPoints.west, drone_position);
 
-        var destination = MapsLib.traceDestinationPoint(posMarkerNorth, distance_in_meter, bearing);
-        console.log(destination);
-        var impactP = new google.maps.Polyline({
+        var direction = cardinalPoints.south;
+        var last_direction;
+
+        // Base y altura
+        var distance_width = MapsLib.getDistanceBetweenPoints(posMarkerNorthWest, posMarkerNorthEast) * 1000;
+        var distance_height = MapsLib.getDistanceBetweenPoints(posMarkerNorthWest, posMarkerSouthWest) * 1000;
+
+        var distance_traveled = CAMERA_TAKE.width / 2;
+        var step;
+        // Dibujar la ruta
+        while (distance_traveled + CAMERA_TAKE.width <= distance_width) {
+            step = (direction == cardinalPoints.west) ? CAMERA_TAKE.width : distance_height;
+            if (direction == cardinalPoints.west && distance_traveled + step > distance_width) {
+                step = distance_width - distance_traveled;
+            }
+
+            drone_position = MapsLib.drawStep(step, direction, drone_position);
+
+            if (direction == cardinalPoints.west) {
+                distance_traveled += CAMERA_TAKE.width;
+            }
+
+            if (direction != cardinalPoints.west) {
+                last_direction = direction;
+                direction = cardinalPoints.west;
+            } else {
+                direction = (last_direction == cardinalPoints.south) ? cardinalPoints.north : cardinalPoints.south;
+            }
+
+        };
+        // Ultimo paso
+        direction = (last_direction == cardinalPoints.south) ? cardinalPoints.north : cardinalPoints.south;
+        var step = (direction == cardinalPoints.west) ? CAMERA_TAKE.width : distance_height;
+        drone_position = MapsLib.drawStep(step, direction, drone_position);
+        // Regreso a casa
+        var return_to_home = new google.maps.Polyline({
             map: map,
-            path: [new google.maps.LatLng(posMarkerNorth.lat, posMarkerNorth.lng),
-            new google.maps.LatLng(destination.lat, destination.lng)
+            path: [drone_position,
+                home
             ],
             strokeColor: "#FF0000",
             strokeOpacity: 1.0,
             strokeWeight: 2
         });
-
-        var markerDest = new google.maps.Marker({
-            position: new google.maps.LatLng(destination.lat, destination.lng),
-            map: map
-        });
+        drone_position = home;
 
 
         console.log(MapsLib.distanceOfRoute(diagonalPath));
     });
-
-
 
 }
 
